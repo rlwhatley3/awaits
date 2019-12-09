d847bcc4a353aadab0e1a8d4b70812846ae60ff6
rlwhatley3 <rlwhatley3@gmail.com>
2019-12-09 17:09:35 -0600

    notation

--- awaits.ts
+++ awaits.ts
@@ -4,22 +4,48 @@ type IhandleSinglePromise = (promise: Promise<any>) => Promise<[null | Error, an
 type IhandleMultiplePromises = (promises: Array<Promise<any>>) => Promise<[null | Error, any]>;
 type Iuntil = (promises: Array<Promise<any>> | Promise<any>) => Promise<[null | Error, any]>;
 
+
+/**
+ * Wraps a given error and outputs the correct tuple
+ * @param error of any type
+ * @return a tuple of type [Error, null]
+ */
 const handleErr: IhandleErr = function handleErr(err: any): [null | Error, any] {
 	return err instanceof Error ? [err, null] : [new Error(err), null];
 }
 
+/**
+ * formats the data return tuple
+ * @param data of any type
+ * @return a tuple of type [null, any]
+ */
 const handleData: IhandleData = function handleData(data: any): [null, any] {
 	return [null, data];
 }
 
+/**
+ * resolves a single given promise
+ * @param data of any type
+ * @return a Promise that resolves to a tuple
+ */
 const handleSinglePromise: IhandleSinglePromise = function handleSinglePromise(promise: Promise<any>): Promise<[null | Error, any]> {
 	return promise.then(handleData, handleErr);
 }
 
+/**
+ * resolves all promises given
+ * @param an array of Promises
+ * @return a Promise that resolves to a tuple
+ */
 const handleMultiplePromises: IhandleMultiplePromises = function handleMultiplePromises(promises: Array<Promise<any>>): Promise<[null | Error, any]> {
 	return Promise.all(promises).then(handleData, handleErr);
 }
 
+/**
+ * resolves a promise or promises then returns a formatted tuple
+ * @param a promise or an array of promises to be resolved
+ * @return a Promise which resolves to a tuple of type [null | Error, any]
+ */
 export function until(promises: Array<Promise<any>> | Promise<any>): Promise<[null | Error, any]> {
 	if(Array.isArray(promises)) {
 		const isAllPromises = promises.every(p => typeof p.then === 'function');
@@ -32,6 +58,11 @@ export function until(promises: Array<Promise<any>> | Promise<any>): Promise<[nu
 	}
 }
 
+/**
+ * resolves a promise or promises then returns a formatted tuple
+ * @param a promise or an array of promises to be resolved
+ * @return a Promise which resolves to a tuple of type [null | Error, any]
+ */
 export function s(promises: Array<Promise<any>> | Promise<any>): Promise<[null | Error, any]> {
 	return until(promises);
 }
